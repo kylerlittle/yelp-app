242d4ae070070ba9fac5f1aa023a69946726ea3d
Navdeep <navdeep.singh@wsu.edu>
2019-02-05 22:59:38 -0800

    Add business list (complete, but needs styling)

--- prototype/frontend/script.js
+++ prototype/frontend/script.js
@@ -4,12 +4,16 @@ const baseurl = 'http://localhost:3000/';
 function start() {
     let stateDropdown = document.getElementById("stateDropdown"),
         cityDropdown = document.getElementById("cityDropdown");
+        businessList = document.getElementById('businessList');
 
     stateDropdown.innerHTML = '';
     cityDropdown.innerHTML = '';
+    businessList.innerHTML = ''; 
 
     getStates(); // Populate stateDropdown with states
     stateDropdown.onchange = getCitiesEvent; // Update cities when stateDropdown changes
+    cityDropdown.onchange = getBusinessesEvent; // Update businesses when cityDropdown changes
+
  }
 
 // Make GET request to recieve states and populate states
@@ -59,8 +63,11 @@ function getCitiesEvent(event) {
 
 // Populate City Dropdown with cities
 function populateCities(cities) {
-    let cityDropdown = document.getElementById("cityDropdown");
+    let cityDropdown = document.getElementById("cityDropdown"),
+        businessList = document.getElementById('businessList');
+
     cityDropdown.innerHTML = ''; // Delete Children
+    businessList.innerHTML = ''; // Reset Business List
     console.log(cities);
 
     for (let i = 0; i < cities.length; i++) {
@@ -70,6 +77,39 @@ function populateCities(cities) {
         cityDropdown.appendChild(s);
     }
     document.getElementById("cityDropdown").selectedIndex = -1; // Default city is set to empty
+}
+
+// Make GET request to update businesses after cityDropdown changes
+function getBusinessesEvent(event) {
+    let city = this.value,
+        state = document.getElementById("stateDropdown").value;
+
+    if (isEmpty(city) || isEmpty(state))
+        return;
+
+    fetch(baseurl + state + '/' + city, {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(response => populateBusinesses(response))
+    .catch(error => console.error('Error:', error));
+}
+
+// Populate businesses
+function populateBusinesses(businesses) {
+    let businessList = document.getElementById('businessList');
+    businessList.innerHTML = ''; // Delete Children
+
+    console.log(businesses);
+
+    for (let i = 0; i < businesses.length; i++) {
+        let s = document.createElement('p');
+        s.innerHTML = businesses[i].business_name;
+        businessList.appendChild(s);
+    }
 }
 
 function isEmpty(str) {
