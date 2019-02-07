d35201de632bb7c3c2ce6c9edfc03b8eb602f356
Charlie Hanacek <charlie@czhanacek.com>
2019-02-07 07:32:40 -0800

    checkins and users complete

--- parsing/parseYelp.js
+++ parsing/parseYelp.js
@@ -3,12 +3,15 @@ const readline = require('readline');
 
 // Parse yelp_review.json and print data to console
 function parseReview() {
-    
+    console.log("REVIEWS");
     const rl = readline.createInterface({
         input: fs.createReadStream('yelp_review.json')
     });
     
     rl.on('line', (line) => {
+        if(line.length < 2) {
+            return; // don't parse a blank line
+        }
         const review = JSON.parse(line);
 
         console.log(`review_id: ${review.review_id},`);
@@ -26,7 +29,7 @@ function parseReview() {
 
 // Parse business.json and print data to console
 function parseBusiness() {
-    
+    console.log("BUSINESSES");
     const rl = readline.createInterface({
         input: fs.createReadStream('yelp_business.json')
     });
@@ -70,5 +73,74 @@ function parseBusinessAttributes(attributes) {
     }
 }
 
+function parseCheckin() {
+    console.log("CHECKINS");
+    const rl = readline.createInterface({
+        input: fs.createReadStream('yelp_checkin.json')
+    });
+    
+    rl.on('line', (line) => {
+        const checkin = JSON.parse(line);
+        console.log(`business_id: ${checkin.business_id},`);
+        console.log(`times: `);
+        for(var dayOfWeek in checkin.time) {
+            console.log(`${dayOfWeek}`);
+            for(var time in checkin.time[dayOfWeek]) {
+                console.log(`${time}: ${checkin.time[dayOfWeek][time]}`);
+            }
+        }
+
+        console.log("-----");
+    });
+}
+
+function parseUser() {
+    console.log("USERS");
+    const rl = readline.createInterface({
+        input: fs.createReadStream('yelp_user.json')
+    });
+    rl.on('line', (line) => {
+        const user = JSON.parse(line);
+        console.log(`Name: ${user.name}`);
+        console.log(`user_id: ${user.user_id}`);
+        console.log(`review_count: ${user.review_count}`);
+        console.log(`useful: ${user.useful}`);
+        console.log(`yelping_since: ${user.yelping_since}`);
+        console.log(`average_stars: ${user.average_stars}`);
+        console.log(`compliment_cool: ${user.compliment_cool}`);
+        console.log(`compliment_cute: ${user.compliment_cute}`);
+        console.log(`compliment_funny: ${user.compliment_funny}`);
+        console.log(`compliment_hot: ${user.compliment_hot}`);
+        console.log(`compliment_list: ${user.compliment_list}`);
+        console.log(`compliment_more: ${user.compliment_more}`);
+        console.log(`compliment_note: ${user.compliment_note}`);
+        console.log(`compliment_photos: ${user.compliment_photos}`);
+        console.log(`compliment_plain: ${user.compliment_plain}`);
+        console.log(`compliment_profile: ${user.compliment_profile}`);
+        console.log(`compliment_writer: ${user.compliment_writer}`);
+        console.log(`cool: ${user.cool}`);
+        console.log(`years elite: `);
+        for(var eliteYear in user.elite) {
+            console.log(`- ${user.elite[eliteYear]}`);
+        }
+        console.log(`friends: `);
+        for(var friend in user.friends) {
+            console.log(`${[friend]}) ${user.friends[friend]}`);
+        }
+        console.log(`fans: ${user.fans}`);
+        console.log(`funny: ${user.funny}`);
+        console.log("------");
+        
+
+
+        
+    
+    });
+
+}
+
+
 parseReview();
 parseBusiness();
+parseCheckin();
+parseUser();
