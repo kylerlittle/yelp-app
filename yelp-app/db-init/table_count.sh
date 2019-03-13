25174c856c632318e5023c4fe8cb4de473f0e2c6
Kyler Little <kylerlittle@Kylers-MacBook-Air.local>
2019-03-13 10:19:54 -0700

    comment to explain regex

--- yelp-app/db-init/table_count.sh
+++ yelp-app/db-init/table_count.sh
@@ -12,6 +12,7 @@ echo -n "" > $OUTPUTFILE
 psql CraKeN_YelpDB -tAc "\dt" | \
 while read i
 do
+    # Perl regex grabs capturing group after first '|'.
     match=`echo $i | perl -nle 'print $1 if m{.+\|\K(.+)(?=\|.+\|\w)}'`
     if [[ match ]]; then
         echo -n "$match table size: " >> $OUTPUTFILE
