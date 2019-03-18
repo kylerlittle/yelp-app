2a2131f10a641ecaa115f7dd37f945b88acec2e3
Kyler Little <kylerlittle@Kylers-MacBook-Air.local>
2019-03-17 22:41:38 -0700

    add onclick event handler for choice; defaults to undefined if no props passed

--- yelp-app/client/src/Choice.js
+++ yelp-app/client/src/Choice.js
@@ -4,7 +4,7 @@ import './Choice.css';
 class Choice extends Component {
   render() {
     return (
-      <div className="Choice">
+      <div className="Choice" onClick={this.props.handleClick}>
         <li>{this.props.children}</li>
       </div>
     );
