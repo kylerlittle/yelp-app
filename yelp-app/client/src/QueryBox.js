50802bb34349aea023de1775d98959b80a5a3470
Kyler Little <kylerlittle@wless-user-172019219207.temp.wsu.edu>
2019-03-19 14:34:33 -0700

    add exit button -- needs work

--- yelp-app/client/src/QueryBox.js
+++ yelp-app/client/src/QueryBox.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import Choice from './Choice';
+import './QueryBox.css'
 
 class QueryBox extends Component {
   render() {
     const formattedQueryAttributes = Object.keys(this.props.selectedQueryAttributes).map((category, index) =>
-        <Choice> {`${category}: ` + this.props.selectedQueryAttributes[category]} </Choice>
+        <Choice>
+            {`${category}: ` + this.props.selectedQueryAttributes[category]}
+            <button type="button" className="close removeQueryAttr" aria-label="Close">
+            <span aria-hidden="true">×</span>
+            </button>
+        </Choice>
+        
     );
     return (
       <div className="QueryBox">
